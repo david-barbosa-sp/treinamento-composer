8311cfdafade7ab01eb81e6f1861c0e96262ab42
David Barbosa <davidbarbosa08@gmail.com>
2023-02-18 11:40:44 -0300

    Preparando o projeto para publicação

--- src/Buscador.php
+++ src/Buscador.php
@@ -10,28 +10,29 @@ class Buscador
     /**
      * @var ClientInterface
      */
-    private $httpClient;
+    private ClientInterface $httpClient;
 
     /**
      * @var Crawler
      */
-    private $crawler;
-   public function __construct(ClientInterface $httpClient, Crawler $crawler)
-   {
-       $this->httpClient = $httpClient;
-       $this->crawler = $crawler;
-   }
+    private Crawler $crawler;
+
+    public function __construct(ClientInterface $httpClient, Crawler $crawler)
+    {
+        $this->httpClient = $httpClient;
+        $this->crawler = $crawler;
+    }
 
     public function buscar(string $url): array
     {
-        $resposta = $this->httpClient->request('get', $url);
+        $resposta = $this->httpClient->request('GET', $url);
         $html = $resposta->getBody();
         $this->crawler->addHtmlContent($html);
         $elementosCursos = $this->crawler->filter('span.card-curso__nome');
         $cursos = [];
-        foreach ($elementosCursos as $elemento){
+        foreach ($elementosCursos as $elemento) {
             $cursos[] = $elemento->textContent;
         }
         return $cursos;
-   }
+    }
 }
